73589693b080394f5ec4af6354f1a7da97709d8f
Larysa Ivshyna <ivshynalarysa@gmail.com>
2026-08-31 00:28:39 +0100

    correct way1

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,5 +1,5 @@
 import NotePreviewClient from './NotePreview.client';
-import { fetchNoteById } from '@/lib/api';
+import { fetchNoteById } from '@/lib/api/clientApi';
 import {
   QueryClient,
   HydrationBoundary,
